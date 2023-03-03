10c125ec098db58bfff8f228fca9c3fdae50f504
fistaco <fiske_schijlen@hotmail.com>
2023-03-04 00:19:36 +0100

    Implement a simple use case as a driver program.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,19 @@
-// BinaryGeneticAlgorithm.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <string>
-#include "GeneticAlgorithm.h"
 #include "BinaryString.h"
+#include "CrossoverType.h"
+#include "GeneticAlgorithm.h"
+#include "Genome.h"
 
 int main()
 {
-    int popSize = 3;
+    int popSize = 8;
+    int gens = 30;
+    auto crossoverType = CrossoverType::OnePoint;
+    float crossoverRate = 0.5f;
+    float mutRate = 0.8f;
+    float maxFitness = 8.0f;
 
-    GeneticAlgorithm* ga = new GeneticAlgorithm(popSize);
-    ga->run();
+    auto ga = GeneticAlgorithm(popSize, gens, crossoverType, crossoverRate, mutRate, maxFitness);
+    ga.run<BinaryString>(8);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
