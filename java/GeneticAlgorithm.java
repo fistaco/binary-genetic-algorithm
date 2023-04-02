b407a9c95b46d491d337cb4025a684dfdcff042e
fistaco <fiske_schijlen@hotmail.com>
2023-04-02 19:17:38 +0200

    Incorporate the crossover type in the GA skeleton.

--- java/GeneticAlgorithm.java
+++ java/GeneticAlgorithm.java
@@ -2,15 +2,21 @@ public class GeneticAlgorithm {
 
     private int popSize;
     private int gens;
+    private CrossoverType crossoverType;
     private float crossoverRate;
     private float mutationRate;
     private float optimalFitness;
 
-    public GeneticAlgorithm(int popSize, int gens, float crossoverRate, float mutationRate, float optimalFitness) {
+    public GeneticAlgorithm(int popSize, int gens, CrossoverType crossoverType, float crossoverRate, float mutationRate, float optimalFitness) {
         this.popSize = popSize;
         this.gens = gens;
+        this.crossoverType = crossoverType;
         this.crossoverRate = crossoverRate;
         this.mutationRate = mutationRate;
         this.optimalFitness = optimalFitness;
+    }
+
+    public void run() {
+
     }
 }
